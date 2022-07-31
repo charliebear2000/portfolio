ab28bf2d598bac20aaf977d7e00a2a4176fb92dd
Leah <amelia_rawls@ccpsnet.net>
2022-07-31 14:43:10 -0400

    added project 3

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,8 +9,16 @@ function Projects() {
          <div className="top-container">Click on each image to go to the live website. Click on the title of each project to go to the GitHub website.</div>
          <div className="project-container">
             <div className="inner-project-container">
+            <div className="project-item">
+                  <a className="project" id="project-1" href="https://bistro-box.herokuapp.com/">
+                  </a>
+                  <a className="project-link" href="https://github.com/charliebear2000/BistroBox">
+                     BistroBox &emsp;<img src="https://img.icons8.com/metro/26/000000/github.png" alt=""/>
+                  </a>
+               </div>
+
                <div className="project-item">
-                  <a className="project" id="project-1" href="https://ckoehler16.github.io/Cocktail-Generator/">
+                  <a className="project" id="project-2" href="https://ckoehler16.github.io/Cocktail-Generator/">
                   </a>
                   <a className="project-link" href="https://github.com/ckoehler16/Cocktail-Generator">
                      Flute and Fork &emsp;<img className="icon" src="https://img.icons8.com/metro/26/000000/github.png" alt=""/>
@@ -18,7 +26,7 @@ function Projects() {
                </div>
 
                <div className="project-item">
-                  <a className="project" id="project-2" href="https://shrouded-gorge-64455.herokuapp.com/">
+                  <a className="project" id="project-3" href="https://shrouded-gorge-64455.herokuapp.com/">
                   </a>
                   <a className="project-link" href="https://github.com/charliebear2000/Food-andDrink-DB">
                      The Recipe Pantry &emsp;<img src="https://img.icons8.com/metro/26/000000/github.png" alt=""/>
@@ -26,7 +34,7 @@ function Projects() {
                </div>
 
                <div className="project-item">
-                  <a className="project" id="project-3" href="https://salty-cove-18833.herokuapp.com/">
+                  <a className="project" id="project-4" href="https://salty-cove-18833.herokuapp.com/">
                   </a>
                   <a className="project-link" href="https://github.com/charliebear2000/budget-tracker">
                      Budget Tracker &emsp;<img src="https://img.icons8.com/metro/26/000000/github.png" alt=""/>
@@ -34,7 +42,7 @@ function Projects() {
                </div>
 
                <div className="project-item">
-                  <a className="project" id="project-4" href="https://charliebear2000.github.io/java-quiz/">
+                  <a className="project" id="project-5" href="https://charliebear2000.github.io/java-quiz/">
                   </a>
                   <a className="project-link" href="https://github.com/charliebear2000/java-quiz">
                      JavaScript Quiz &emsp;<img src="https://img.icons8.com/metro/26/000000/github.png" alt=""/>
@@ -42,18 +50,10 @@ function Projects() {
                </div>
 
                <div className="project-item">
-                  <a className="project" id="project-5" href="https://limitless-everglades-40036.herokuapp.com/">
+                  <a className="project" id="project-6" href="https://limitless-everglades-40036.herokuapp.com/">
                   </a>
                   <a className="project-link" href="https://github.com/charliebear2000/Note-Taker">
                      Note-Taker &emsp;<img src="https://img.icons8.com/metro/26/000000/github.png" alt=""/>
-                  </a>
-               </div>
-
-               <div className="project-item">
-                  <a className="project" id="project-6" href="https://charliebear2000.github.io/workday-scheduler/">
-                  </a>
-                  <a className="project-link" href="https://github.com/charliebear2000/workday-scheduler">
-                     Workday Scheduler &emsp;<img src="https://img.icons8.com/metro/26/000000/github.png" alt=""/>
                   </a>
                </div>
 
